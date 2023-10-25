9b3afb8bcf81ba89dcc02688357bf5d27b9df0fe
subbusara <sudhaparvatham@gmail.com>
2023-10-25 15:25:56 +0530

    projects

---
+++ src/com/ags/DemoEnum.java
@@ -0,0 +1,32 @@
+package com.ags;
+
+import java.util.ArrayList;
+
+public class DemoEnum {
+	
+	public enum days{
+		SUNDAY,MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY
+	}
+	public static void main(String[]args) {
+	
+		days[] values = days.values();
+		int length = values.length;
+		System.out.println(values[2]);
+		
+		//normal for loop
+		
+		for(int i=0; i<values.length;i++)
+		{
+			System.out.println(values[i]);
+			
+		}
+		//using enchanced forloop
+		for
+		(days days:values) {
+			System.out.println(days);
+		}
+		
+		
+	}
+
+}
---
+++ src/com/ags/StudentDetails.java
@@ -0,0 +1,18 @@
+package com.ags;
+
+public class StudentDetails {
+	private void studName() {
+		System.out.println ("student name is karan");
+		}
+	private void studClass() {
+		System.out.println ("student class is 5th");
+		}
+	
+	public static void main(String [] args) {
+		StudentDetails s=new StudentDetails();
+				s.studName();
+				s.studClass();
+				
+	}
+
+}
